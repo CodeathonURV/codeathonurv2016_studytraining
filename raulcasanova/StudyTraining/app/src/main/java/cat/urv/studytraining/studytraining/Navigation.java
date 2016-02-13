db8fcb1017b004b409cc10f472ad5a000a3a0cff
Raúl Casanova Marqués <raul.casanova@estudiants.urv.cat>
2016-02-13 13:07:33 +0100

    raulcasanova: added scoreboard list

--- raulcasanova/StudyTraining/app/src/main/java/cat/urv/studytraining/studytraining/Navigation.java
+++ raulcasanova/StudyTraining/app/src/main/java/cat/urv/studytraining/studytraining/Navigation.java
@@ -1,9 +1,6 @@
 package cat.urv.studytraining.studytraining;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -12,11 +9,39 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.SimpleAdapter;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Navigation extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+
+    private static final String KEY_NAME = "KEY_NAME";
+    private static final String KEY_SCORE = "KEY_SCORE";
+
+    private static final String[] staticName = {
+            "Raúl Casanova Marqués",
+            "Jordi Merino Domingo",
+            "Oriol Mauri Guiu",
+            "Ana Lopez Rubio",
+            "David Reyes Garcia",
+            "Silvia Lopez Clotet",
+            "Nuria Fernandez Diaz",
+            "Diego Hernandez Castillo"
+    };
+
+    private static final String[] staticScore = {
+            "12543",
+            "8546",
+            "7456",
+            "4234",
+            "3215",
+            "2415",
+            "1234",
+            "246"
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,34 +60,19 @@ public class Navigation extends AppCompatActivity
         navigationView.setNavigationItemSelectedListener(this);
 
 
-        // Get ListView object from xml
-        ListView listScoreBoard = (ListView) findViewById(R.id.listScoreBoard);
+        ListView listScore = (ListView) findViewById(R.id.listScoreBoard);
 
-        // Defined Array defaultValues to show in ListView
-        String[] defaultValues = new String[] {
-                "Raúl Casanova Marqués",
-                "Jordi Merino Domingo",
-                "Oriol Mauri Guiu",
-                "Ana Lopez Rubio",
-                "David Reyes Garcia",
-                "Silvia Lopez Clotet",
-                "Nuria Fernandez Diaz",
-                "Diego Hernandez Castillo"
-        };
+        ArrayList<HashMap<String, String>> listScoreBoard = new ArrayList<>();
+        HashMap<String, String> values;
+        for (int i = 0; i < staticName.length; i++) {
+            values = new HashMap<>();
+            values.put(KEY_NAME, staticName[i]);
+            values.put(KEY_SCORE, staticScore[i]);
+            listScoreBoard.add(values);
+        }
 
-        // Define a new Adapter
-        // First parameter - Context
-        // Second parameter - Layout for the row
-        // Third parameter - ID of the TextView to which the data is written
-        // Forth - the Array of data
-
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
-                android.R.layout.simple_list_item_1, android.R.id.text1, defaultValues);
-
-
-        // Assign adapter to ListView
-        listScoreBoard.setAdapter(adapter);
-
+        SimpleAdapter adapter = new SimpleAdapter(this, listScoreBoard, R.layout.content_navigation, new String[]{KEY_NAME, KEY_SCORE}, new int[]{R.id.name_ScoreBoard, R.id.score_ScoreBoard});
+        listScore.setAdapter(adapter);
 
     }
 
