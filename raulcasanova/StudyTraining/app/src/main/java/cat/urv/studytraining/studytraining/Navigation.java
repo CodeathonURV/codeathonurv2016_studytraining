491fa4afc72c493b66da6cccb84a8bab7a63d45d
Raúl Casanova Marqués <raul.casanova@estudiants.urv.cat>
2016-02-13 14:31:26 +0100

    raulcasanova: scoreboard implementation (ScoreBoardActivity)

--- raulcasanova/StudyTraining/app/src/main/java/cat/urv/studytraining/studytraining/Navigation.java
+++ raulcasanova/StudyTraining/app/src/main/java/cat/urv/studytraining/studytraining/Navigation.java
@@ -1,5 +1,6 @@
 package cat.urv.studytraining.studytraining;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -9,39 +10,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.ListView;
-import android.widget.SimpleAdapter;
-
-import java.util.ArrayList;
-import java.util.HashMap;
 
 public class Navigation extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
-    private static final String KEY_NAME = "KEY_NAME";
-    private static final String KEY_SCORE = "KEY_SCORE";
-
-    private static final String[] staticName = {
-            "Raúl Casanova Marqués",
-            "Jordi Merino Domingo",
-            "Oriol Mauri Guiu",
-            "Ana Lopez Rubio",
-            "David Reyes Garcia",
-            "Silvia Lopez Clotet",
-            "Nuria Fernandez Diaz",
-            "Diego Hernandez Castillo"
-    };
-
-    private static final String[] staticScore = {
-            "12543",
-            "8546",
-            "7456",
-            "4234",
-            "3215",
-            "2415",
-            "1234",
-            "246"
-    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -58,22 +30,6 @@ public class Navigation extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
-
-
-        ListView listScore = (ListView) findViewById(R.id.listScoreBoard);
-
-        ArrayList<HashMap<String, String>> listScoreBoard = new ArrayList<>();
-        HashMap<String, String> values;
-        for (int i = 0; i < staticName.length; i++) {
-            values = new HashMap<>();
-            values.put(KEY_NAME, staticName[i]);
-            values.put(KEY_SCORE, staticScore[i]);
-            listScoreBoard.add(values);
-        }
-
-        SimpleAdapter adapter = new SimpleAdapter(this, listScoreBoard, R.layout.content_navigation, new String[]{KEY_NAME, KEY_SCORE}, new int[]{R.id.name_ScoreBoard, R.id.score_ScoreBoard});
-        listScore.setAdapter(adapter);
-
     }
 
     @Override
@@ -114,17 +70,12 @@ public class Navigation extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        if (id == R.id.nav_globalscore) {
+            Intent myIntent = new Intent(Navigation.this, ScoreBoardActivity.class);
+            startActivityForResult(myIntent, 0);
+        } else if (id == R.id.nav_info) {
 
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.nav_infoscore) {
 
         }
 
